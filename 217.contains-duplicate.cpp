01fd2e605efa138f721104fcf7ed8b872aa084c7
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2023-06-08 01:25:21 +0530

    -

--- 217.contains-duplicate.cpp
+++ 217.contains-duplicate.cpp
@@ -7,8 +7,13 @@
 // @lc code=start
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        
+    bool containsDuplicate(vector<int>& v) {
+        sort(v.begin(), v.end());
+        for (int i=0; i<v.size()-1; i++) {
+            if (v[i] == v[i+1])
+                return true;
+        }
+        return false;
     }
 };
 // @lc code=end
