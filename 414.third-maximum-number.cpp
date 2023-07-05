f6f3413de88a3d90315d328484d813dfd0c95aa0
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2023-07-06 04:19:58 +0530

    .

--- 414.third-maximum-number.cpp
+++ 414.third-maximum-number.cpp
@@ -8,29 +8,36 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        int firstMax = INT_MIN;
+        long firstMax = LONG_MIN, secondMax = LONG_MIN, thirdMax = LONG_MIN;
         int n = nums.size();
         
+        if (n==1)
+            return nums[0];
+        else if (n==2)
+            return max(nums[0], nums[1]);
+
         for (int i=0; i<n; i++) {
-            if (firstMax < nums[i])
+            if (nums[i] == firstMax || nums[i] == secondMax || nums[i] == thirdMax)
+                continue;
+
+            if (firstMax < nums[i]) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
                 firstMax = nums[i];
-        }
-
-        int secondMax = firstMax;
-        for (int i=0; i<n; i++) {
-            if (secondMax > nums[i] && nums[i] <= firstMax) {
+            }
+            else if (secondMax < nums[i] && secondMax < firstMax) {
+                thirdMax = secondMax;
                 secondMax = nums[i];
-                break;
+            }
+            else if (thirdMax < nums[i] && thirdMax < secondMax) {
+                thirdMax = nums[i];
             }
         }
 
-        int thirdMax = secondMax;
-        for (int i=0; i<n; i++) {
-            if (thirdMax > nums[i] && nums[i] <= secondMax) {
-                thirdMax = nums[i];
-                break;
-            }
-        }
+        if (thirdMax == LONG_MIN)
+            return firstMax;
+        if (secondMax == LONG_MIN)
+            return firstMax;
 
         return thirdMax;
     }
