2dc73b1ab5e95116d5586139f0bc254d3fd0556e
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2023-07-23 20:46:03 +0530

    -

---
+++ 2733. Neither Minimum nor Maximum.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=2733 lang=cpp
+ *
+ * 2733. Neither Minimum nor Maximum
+ */
+// @lc code=start
+
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        int max = -1;
+        int min = 101;
+        int n=nums.size();
+        
+        for (int i=0; i<n; i++) {
+            if (max < nums[i])
+                max = nums[i];
+            if (min > nums[i])
+                min = nums[i];
+        }
+
+        for (int i=0; i<n; i++) {
+            if (nums[i] != max && nums[i] != min)
+                return nums[i];
+        }
+        
+        return -1;
+    }
+};
+
