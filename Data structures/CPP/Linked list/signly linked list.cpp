541d87cb73fbbe3e826cdeb918e8c7eb8316e480
hexadivine <hexadivine@gmail.com>
2024-02-26 18:56:50 +1100

    created a node

---
+++ Data structures/CPP/Linked list/signly linked list.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+
+        Node(int val) {
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+int main() {
+
+    Node *head = new Node(10);
+
+    cout << head->val << endl;
+    cout << head->next;
+
+    return 0;
+}
