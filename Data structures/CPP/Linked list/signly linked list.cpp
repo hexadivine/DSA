8d3cd3de88c5434777a01691c91a13aabdad0548
hexadivine <hexadivine@gmail.com>
2024-02-26 18:59:56 +1100

    testing functions

--- Data structures/CPP/Linked list/signly linked list.cpp
+++ Data structures/CPP/Linked list/signly linked list.cpp
@@ -25,11 +25,23 @@ void print(Node* head) {
         cout << head->val << " ";
         head = head->next;
     }
+    cout << endl;
 }
 
 int main() {
 
     Node* head = new Node(10);
+
+    insertToLeft(head, 9);
+    insertToLeft(head, 8);
+    insertToLeft(head, 7);
+
+    print(head);
+
+    insertToLeft(head, 6);
+    insertToLeft(head, 5);
+
+    print(head);
 
     return 0;
 }
