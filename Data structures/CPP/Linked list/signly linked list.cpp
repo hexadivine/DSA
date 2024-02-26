751269c6f880d87c165717dcceebfc19b3dffebd
hexadivine <hexadivine@gmail.com>
2024-02-26 18:58:03 +1100

    added print functionality

--- Data structures/CPP/Linked list/signly linked list.cpp
+++ Data structures/CPP/Linked list/signly linked list.cpp
@@ -17,15 +17,19 @@ void insertToLeft(Node* &head, int val) {
     Node* temp = new Node(val);
     temp->next = head;
     head = temp;
-    
+
+}
+
+void print(Node* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
 }
 
 int main() {
 
     Node* head = new Node(10);
-
-    cout << head->val << endl;
-    cout << head->next;
 
     return 0;
 }
